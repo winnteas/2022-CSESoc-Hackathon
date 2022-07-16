e14dd470a61267edee818442e0f19186f8ca8009
Olivia <olivia.tardjono@gmail.com>
2022-07-17 00:23:17 +1000

    Percentage of likes and dislikes

--- likes.js
+++ likes.js
@@ -5,7 +5,9 @@ let likes = 0;
 likeBtn.addEventListener("click", function() {
     // Adds a like
     likes = likes + 1;
+    // FIX - to show this later in frontend
     alert(likes)
+    calcPercentLikes(likes, dislikes)
 });
 
 const dislikeBtn = document.getElementById("dislikeBtn");
@@ -14,5 +16,16 @@ let dislikes = 0;
 dislikeBtn.addEventListener("click", function() {
     // Adds a dislike
     dislikes = dislikes + 1;
+    // FIX - to show this later in frontend
     alert(dislikes);
+    calcPercentLikes(likes, dislikes);
 });
+
+var percentage = 0;
+
+// Calculating percentage for likes
+function calcPercentLikes(likes, dislikes) {
+    percent_likes = likes / (likes + dislikes) * 100;
+    percent_dislikes = dislikes / (likes + dislikes) * 100;
+    alert("The percentage of likes is: " + percent_likes + ".\nThe percentage of dislikes is: " + percent_dislikes + ".")
+}
