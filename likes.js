b3ef9cd262a94cee26e188dce5fe119e1fa6bae3
Dhruv <dhruv.gravity@gmail.com>
2022-07-17 01:42:14 +1000

    added some stuff

--- likes.js
+++ likes.js
@@ -1,29 +1,46 @@
 // Calculating Likes
-const likeBtn = document.getElementById("likeBtn");
+const like_btn = document.getElementById("like_btn");
 let likes = 0;
 
-likeBtn.addEventListener("click", function() {
+like_btn.addEventListener("click", function() {
     // Adds a like
     likes = likes + 1;
     // FIX - to show this later in frontend
-    alert(likes)
+    display();
     calcPercentLikes(likes, dislikes)
 });
 
-const dislikeBtn = document.getElementById("dislikeBtn");
+const dislike_btn = document.getElementById("dislike_btn");
 let dislikes = 0;
 
-dislikeBtn.addEventListener("click", function() {
+dislike_btn.addEventListener("click", function() {
     // Adds a dislike
     dislikes = dislikes + 1;
     // FIX - to show this later in frontend
-    alert(dislikes);
+    display();
     calcPercentLikes(likes, dislikes);
 });
 
+back_button_statistics.addEventListener("click", function() {
+    document.getElementById("like_interface").style.display = "block";
+    document.getElementById("statistics_interface").style.display = "none";
+});
+
 // Calculating percentage for likes
-function calcPercentLikes(likes, dislikes) {
+function calcPercentLikes() {
     percent_likes = likes / (likes + dislikes) * 100;
     percent_dislikes = dislikes / (likes + dislikes) * 100;
-    alert("The percentage of likes is: " + percent_likes + ".\nThe percentage of dislikes is: " + percent_dislikes + ".")
+    return percent_likes;
+}
+
+
+function display() {
+    document.getElementById("like_interface").style.display = "none";
+    document.getElementById("statistics_interface").style.display = "block";
+    document.getElementById("like_count").innerHTML = likes;
+    document.getElementById("dislike_count").innerHTML = dislikes;
+    alert(calcPercentLikes().toString());
+    document.getElementById("like_ratio_1").setAttribute("offset", calcPercentLikes().toString());
+    document.getElementById("like_ratio_2").setAttribute("offset", calcPercentLikes().toString());
+
 }
