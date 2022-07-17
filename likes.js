293252b322a3b184773eb80fa3761d0448fe40c9
Dhruv <dhruv.gravity@gmail.com>
2022-07-17 10:18:56 +1000

    added more

--- likes.js
+++ likes.js
@@ -1,6 +1,6 @@
 // Calculating Likes
 const like_btn = document.getElementById("like_btn");
-let likes = 0;
+let likes = 35678;
 
 like_btn.addEventListener("click", function() {
     // Adds a like
@@ -11,7 +11,7 @@ like_btn.addEventListener("click", function() {
 });
 
 const dislike_btn = document.getElementById("dislike_btn");
-let dislikes = 0;
+let dislikes = 8999;
 
 dislike_btn.addEventListener("click", function() {
     // Adds a dislike
@@ -39,8 +39,4 @@ function display() {
     document.getElementById("statistics_interface").style.display = "block";
     document.getElementById("like_count").innerHTML = likes;
     document.getElementById("dislike_count").innerHTML = dislikes;
-    alert(calcPercentLikes().toString());
-    document.getElementById("like_ratio_1").setAttribute("offset", calcPercentLikes().toString());
-    document.getElementById("like_ratio_2").setAttribute("offset", calcPercentLikes().toString());
-
 }
